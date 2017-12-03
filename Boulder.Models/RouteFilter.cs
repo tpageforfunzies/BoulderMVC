b1b84c306c20864679e9f7c0a2bc4a22bcbca50b
Taylor Page <taylor.jay.page@gmail.com>
2017-12-03 16:30:59 -0500

    starting filter/stats, classes added for each

---
+++ Boulder.Models/RouteFilter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boulder.Models
+{
+    public class RouteFilter
+    {
+    }
+}
--- Boulder.Services/RouteService.cs
+++ Boulder.Services/RouteService.cs
@@ -112,5 +112,7 @@ namespace Boulder.Services
                 return db.SaveChanges() == 1;
             }
         }
+
+        
     }
 }
