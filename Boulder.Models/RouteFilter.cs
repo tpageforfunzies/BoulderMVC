ac48a3a3808ef92d4b1a6ab5ea9688e4064804d3
Taylor Page <taylor.jay.page@gmail.com>
2017-12-03 17:21:53 -0500

    routefilter model set up

--- Boulder.Models/RouteFilter.cs
+++ Boulder.Models/RouteFilter.cs
@@ -6,5 +6,10 @@ namespace Boulder.Models
 {
     public class RouteFilter
     {
+        public int HighestGrade { get; set; }
+        public int LowestGrade { get; set; }
+        public int AverageGrade { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
     }
 }
